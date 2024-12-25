b3160421a9641bfa49f5943d2149fc6a718d796a
BlueberryOreo <1761988645@qq.com>
2024-12-25 17:53:42 +0800

    重构收割机控制器，优化路径选择逻辑并移除冗余代码

--- src/harvester_controller.py
+++ src/harvester_controller.py
@@ -33,40 +33,44 @@ def run_harvester(creep: Creep):
     # If we're full, stop filling up and remove the saved source
     if creep.memory.filling and creep.store.getFreeCapacity() <= 0:
         creep.memory.filling = False
-        del creep.memory.source
     # If we're empty, start filling again and remove the saved target
-    elif not creep.memory.filling and creep.carry.energy <= 0:
+    elif not creep.memory.filling and creep.store.getCapacity() <= 0:
         creep.memory.filling = True
-        del creep.memory.target
+    
+    if creep.memory.path == None:
+        source = _.sample(creep.room.find(FIND_SOURCES))
+        spawn = _.sample(creep.room.find(FIND_MY_SPAWNS))
+        path = PathFinder.search(spawn, {"pos": source.pos, "range": 1})
+        creep.memory.path = Room.serializePath(path.path)
 
-    if creep.memory.filling:
-        # If we have a saved source, use it
-        if creep.memory.source:
-            source = Game.getObjectById(creep.memory.source)
-        else:
-            # Get a random new source and save it
-            source = _.sample(creep.room.find(FIND_SOURCES))
-            creep.memory.source = source.id
+    # if creep.memory.filling:
+    #     # If we have a saved source, use it
+    #     if creep.memory.source:
+    #         source = Game.getObjectById(creep.memory.source)
+    #     else:
+    #         # Get a random new source and save it
+    #         source = _.sample(creep.room.find(FIND_SOURCES))
+    #         creep.memory.source = source.id
 
-        # If we're near the source, harvest it - otherwise, move to it.
-        if creep.pos.isNearTo(source):
-            result = creep.harvest(source)
-            if result != OK:
-                logger.warning("[{}] Unknown result from creep.harvest({}): {}".format(creep.name, source, result))
-        else:
-            creep.moveTo(source)
-    else:
-        if creep.memory.target:
-            target = Game.getObjectById(creep.memory.target)
-        else:
-            # Get a random new target and save it
-            target = _.sample(creep.room.find(FIND_MY_SPAWNS))
-            creep.memory.target = target.id
+    #     # If we're near the source, harvest it - otherwise, move to it.
+    #     if creep.pos.isNearTo(source):
+    #         result = creep.harvest(source)
+    #         if result != OK:
+    #             logger.warning("[{}] Unknown result from creep.harvest({}): {}".format(creep.name, source, result))
+    #     else:
+    #         creep.moveTo(source)
+    # else:
+    #     if creep.memory.target:
+    #         target = Game.getObjectById(creep.memory.target)
+    #     else:
+    #         # Get a random new target and save it
+    #         target = _.sample(creep.room.find(FIND_MY_SPAWNS))
+    #         creep.memory.target = target.id
         
-        # If we're near the target, transfer energy to it - otherwise, move to it.
-        if creep.pos.isNearTo(target):
-            result = creep.transfer(target, RESOURCE_ENERGY)
-            if result != OK:
-                logger.warning("[{}] Unknown result from creep.transfer({}): {}".format(creep.name, target, result))
-        else:
-            creep.moveTo(target)
+    #     # If we're near the target, transfer energy to it - otherwise, move to it.
+    #     if creep.pos.isNearTo(target):
+    #         result = creep.transfer(target, RESOURCE_ENERGY)
+    #         if result != OK:
+    #             logger.warning("[{}] Unknown result from creep.transfer({}): {}".format(creep.name, target, result))
+    #     else:
+    #         creep.moveTo(target)
