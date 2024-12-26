4e482768529ff7253fb2bba7203bee21140fd25f
BlueberryOreo <1761988645@qq.com>
2024-12-26 19:38:23 +0800

    优化建筑者控制器的目标选择逻辑，优先选择进度最高的建筑工地

--- src/builder_controller.py
+++ src/builder_controller.py
@@ -38,7 +38,8 @@ def run_builder(creep: Creep):
     if creep.memory.status == S_FINDINGWAY:
         if not creep.memory.path_to or not creep.memory.path_back:
             source = _.sample(creep.room.find(FIND_SOURCES))
-            target = _.sample(creep.room.find(FIND_CONSTRUCTION_SITES))
+            target = creep.room.find(FIND_CONSTRUCTION_SITES)
+            target = _.sortBy(target, lambda t: -t.progress)[0] # Find the construction site with the most progress.
 
             path_to = creep.room.findPath(target.pos, source.pos)
             # path_to = creep.room.findPath(source.pos, target.pos)
